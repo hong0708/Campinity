fa55ead6b127e7f6b39f1370ba563192c6babd27
ubermen5ch1308 <ubermen5ch1308@gmail.com>
2023-01-11 12:18:15 +0900

    [SETTING] 백엔드 프로젝트 개발환경 구축

---
+++ Server/demo-api-server/src/main/java/com/ssafy/campinity/demo/CampinityApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.campinity.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampinityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CampinityApplication.class, args);
+	}
+
+}
---
+++ Server/demo-api-server/src/test/java/com/ssafy/campinity/demo/CampinityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.campinity.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampinityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
