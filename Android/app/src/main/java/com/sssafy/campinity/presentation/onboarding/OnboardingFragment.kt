5259b74e0d856d37ca99226eebf7c6cfe43d0426
윤세림 <tpfla9953@naver.com>
2023-01-13 10:00:34 +0900

    [FEAT] 접근 권한 뷰 구현

---
+++ Android/app/src/main/java/com/sssafy/campinity/presentation/onboarding/OnboardingFragment.kt
@@ -0,0 +1,12 @@
+package com.sssafy.campinity.presentation.onboarding
+
+import com.ssafy.campinity.R
+import com.ssafy.campinity.databinding.FragmentOnboardingBinding
+import com.sssafy.campinity.presentation.base.BaseFragment
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class OnboardingFragment : BaseFragment<FragmentOnboardingBinding>(R.layout.fragment_onboarding) {
+    override fun initView() {
+    }
+}
---
+++ Android/app/src/main/java/com/sssafy/campinity/presentation/onboarding/PermissionFragment.kt
@@ -0,0 +1,14 @@
+package com.sssafy.campinity.presentation.onboarding
+
+import com.ssafy.campinity.R
+import com.ssafy.campinity.databinding.FragmentPermissionBinding
+import com.sssafy.campinity.presentation.base.BaseFragment
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class PermissionFragment : BaseFragment<FragmentPermissionBinding>(R.layout.fragment_permission) {
+
+    override fun initView() {
+
+    }
+}
