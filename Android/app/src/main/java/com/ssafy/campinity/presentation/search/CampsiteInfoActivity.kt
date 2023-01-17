e575c79cffe9cb5cc02ec9b5a458322169b97694
dobbi030 <dmstjs7047@hanmail.net>
2023-01-17 17:57:13 +0900

    [FEAT] 상세 페이지 구현 (#S08P12D101-122)

--- Android/app/src/main/java/com/ssafy/campinity/presentation/search/CampsiteInfoActivity.kt
+++ Android/app/src/main/java/com/ssafy/campinity/presentation/search/CampsiteInfoActivity.kt
@@ -1,26 +1,12 @@
 package com.ssafy.campinity.presentation.search
 
-import android.net.Uri
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.ssafy.campinity.R
-import com.ssafy.campinity.databinding.ActivityCampsiteInfoBinding
 
 class CampsiteInfoActivity : AppCompatActivity() {
-    private val binding: ActivityCampsiteInfoBinding by lazy {
-        ActivityCampsiteInfoBinding.inflate(
-            layoutInflater
-        )
-    }
-    private lateinit var campsiteImages: Array<Uri>
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_campsite_info)
-
-        binding.vpCampingSiteImage.offscreenPageLimit = 1
-        binding.vpCampingSiteImage.adapter = CampsiteImageAdapter(this, campsiteImages)
-
-        //viewpager activity에 연결
     }
 }
