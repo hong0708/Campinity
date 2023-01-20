b4311c5d80c4274c4ac1d89d85c1f9b97a62d60c
HongMinKi <gaogai2@naver.com>
2023-01-20 14:29:59 +0900

    [FEAT] 커뮤니티 쪽지 메인, 자유 쪽지 다이얼로그 디자인 구현

---
+++ Android/app/src/main/java/com/ssafy/campinity/presentation/community/CommunityNoteMyFragment.kt
@@ -0,0 +1,17 @@
+package com.ssafy.campinity.presentation.community
+
+import com.ssafy.campinity.R
+import com.ssafy.campinity.databinding.FragmentCommunityNoteMyBinding
+import com.ssafy.campinity.presentation.base.BaseFragment
+
+class CommunityNoteMyFragment :
+    BaseFragment<FragmentCommunityNoteMyBinding>(R.layout.fragment_community_note_my) {
+
+    override fun initView() {
+        initListener()
+    }
+
+    private fun initListener() {
+
+    }
+}
---
+++ Android/app/src/main/java/com/ssafy/campinity/presentation/community/CommunityNoteQuestionFragment.kt
@@ -0,0 +1,17 @@
+package com.ssafy.campinity.presentation.community
+
+import com.ssafy.campinity.R
+import com.ssafy.campinity.databinding.FragmentCommunityNoteQuestionBinding
+import com.ssafy.campinity.presentation.base.BaseFragment
+
+class CommunityNoteQuestionFragment :
+    BaseFragment<FragmentCommunityNoteQuestionBinding>(R.layout.fragment_community_note_question) {
+
+    override fun initView() {
+        initListener()
+    }
+
+    private fun initListener() {
+
+    }
+}
