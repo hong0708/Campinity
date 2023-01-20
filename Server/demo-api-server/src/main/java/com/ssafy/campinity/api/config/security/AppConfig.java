ab0e4bf823bd2f3ef44965176d6e496e66edbddb
ubermen5ch1308 <ubermen5ch1308@gmail.com>
2023-01-20 15:32:32 +0900

    [BUILD] 소셜로그인에 필요한 의존성 추가 및 설정정보 추가 #(S08P12D101-200)

---
+++ Server/demo-api-server/src/main/java/com/ssafy/campinity/api/config/security/AppConfig.java
@@ -0,0 +1,22 @@
+package com.ssafy.campinity.api.config.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class AppConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.cors().and().csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/user/kakao/callback", "/user/login").permitAll()
+                .anyRequest().authenticated();
+        return http.build();
+    }
+}
