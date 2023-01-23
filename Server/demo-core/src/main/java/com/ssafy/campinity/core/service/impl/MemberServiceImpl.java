f1fd43fe7b9c3782461bd63697106ad310ca4eb2
구미_6반_권용준 <ubermen5ch1308@gmail.com>
2023-01-23 12:14:35 +0900

    Feature be/#190 link kakao login api

---
+++ Server/demo-core/src/main/java/com/ssafy/campinity/core/service/impl/MemberServiceImpl.java
@@ -0,0 +1,37 @@
+package com.ssafy.campinity.core.service.impl;
+
+import com.ssafy.campinity.core.entity.member.Member;
+import com.ssafy.campinity.core.repository.member.MemberRepository;
+import com.ssafy.campinity.core.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.NoSuchElementException;
+import java.util.UUID;
+
+@RequiredArgsConstructor
+@Service
+public class MemberServiceImpl implements MemberService {
+
+    private final MemberRepository memberRepository;
+
+    @Override
+    public Member findMemberById(Integer id) {
+        return memberRepository.findById(id).orElseThrow(NoSuchElementException::new);
+    }
+
+    @Override
+    public Member findMemberByUUID(UUID uuid) {
+        return memberRepository.findMemberByUuidAndExpiredIsFalse(uuid).orElseThrow(NoSuchElementException::new);
+    }
+
+    @Override
+    public Member save(Member member) {
+        return memberRepository.save(member);
+    }
+
+    @Override
+    public Boolean checkNicknameDuplicate(String nickname) {
+        return memberRepository.existsByName(nickname);
+    }
+}
