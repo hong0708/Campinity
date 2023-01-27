aadbae2cf23336977df7f900d2a1d8a7187c57ff
HongMinKi <gaogai2@naver.com>
2023-01-27 18:00:22 +0900

    [FEAT] 커뮤니티 쪽지 메인 리사이클러뷰 연결

--- Android/app/src/main/java/com/ssafy/campinity/presentation/community/CommunityNoteMyFragment.kt
+++ Android/app/src/main/java/com/ssafy/campinity/presentation/community/CommunityNoteMyFragment.kt
@@ -1,5 +1,6 @@
 package com.ssafy.campinity.presentation.community
 
+import android.util.Log
 import com.ssafy.campinity.R
 import com.ssafy.campinity.databinding.FragmentCommunityNoteMyBinding
 import com.ssafy.campinity.presentation.base.BaseFragment
@@ -7,8 +8,12 @@ import com.ssafy.campinity.presentation.base.BaseFragment
 class CommunityNoteMyFragment :
     BaseFragment<FragmentCommunityNoteMyBinding>(R.layout.fragment_community_note_my) {
 
+    private val impl = arrayListOf<String>("a", "b", "c")
+
     override fun initView() {
         initListener()
+        Log.d("왜?", "initView: ")
+        binding.rvCommunityMyNote.adapter = CommunityNoteListAdapter(impl)
     }
 
     private fun initListener() {
--- Android/app/src/main/java/com/ssafy/campinity/presentation/community/CommunityNoteQuestionFragment.kt
+++ Android/app/src/main/java/com/ssafy/campinity/presentation/community/CommunityNoteQuestionFragment.kt
@@ -1,5 +1,6 @@
 package com.ssafy.campinity.presentation.community
 
+import android.util.Log
 import com.ssafy.campinity.R
 import com.ssafy.campinity.databinding.FragmentCommunityNoteQuestionBinding
 import com.ssafy.campinity.presentation.base.BaseFragment
@@ -7,8 +8,12 @@ import com.ssafy.campinity.presentation.base.BaseFragment
 class CommunityNoteQuestionFragment :
     BaseFragment<FragmentCommunityNoteQuestionBinding>(R.layout.fragment_community_note_question) {
 
+    private val impl = arrayListOf<String>("1", "2", "3")
+
     override fun initView() {
         initListener()
+        Log.d("왜?", "initView: ")
+        binding.rvCommunityQuestionNote.adapter = CommunityNoteListAdapter(impl)
     }
 
     private fun initListener() {
