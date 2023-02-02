5e1f374dc00a53cb38384e2b28ca624f7a1983a0
dobbi030 <dmstjs7047@hanmail.net>
2023-02-02 20:32:30 +0900

    [CHORE] Merge conflict 해결

--- Android/app/src/main/java/com/ssafy/campinity/di/RepositoryModule.kt
+++ Android/app/src/main/java/com/ssafy/campinity/di/RepositoryModule.kt
@@ -2,13 +2,13 @@ package com.ssafy.campinity.di
 
 import com.ssafy.campinity.data.remote.datasource.auth.AuthRemoteDataSourceImpl
 import com.ssafy.campinity.data.remote.datasource.collection.CollectionRemoteDataSourceImpl
+import com.ssafy.campinity.data.remote.datasource.curation.CurationRemoteDataSourceImpl
+import com.ssafy.campinity.data.remote.datasource.home.HomeRemoteDataSourceImpl
+import com.ssafy.campinity.data.remote.datasource.note.NoteRemoteDataSourceImpl
+import com.ssafy.campinity.data.remote.datasource.search.SearchRemoteDataSourceImpl
 import com.ssafy.campinity.data.remote.datasource.user.UserRemoteDataSourceImpl
-import com.ssafy.campinity.data.remote.repository.AuthRepositoryImpl
-import com.ssafy.campinity.data.remote.repository.CollectionRepositoryImpl
-import com.ssafy.campinity.data.remote.repository.UserRepositoryImpl
-import com.ssafy.campinity.domain.repository.AuthRepository
-import com.ssafy.campinity.domain.repository.CollectionRepository
-import com.ssafy.campinity.domain.repository.UserRepository
+import com.ssafy.campinity.data.remote.repository.*
+import com.ssafy.campinity.domain.repository.*
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
