17b281c80c92b2e5a0c5ca0f9e7c7523bc3497f0
serim53 <tpfla9953@naver.com>
2023-02-05 23:54:17 +0900

    [CHORE] 코드 클린업

--- Android/app/src/main/java/com/ssafy/campinity/common/util/Extension.kt
+++ Android/app/src/main/java/com/ssafy/campinity/common/util/Extension.kt
@@ -22,7 +22,6 @@ fun getDeviceHeightPx(context: Context): Int {
 @SuppressLint("InternalInsetResource")
 fun getStatusBarHeight(context: Context): Int {
     val resourceId = context.resources.getIdentifier("status_bar_height", "dimen", "android")
-
     return if (resourceId > 0) {
         context.resources.getDimensionPixelSize(resourceId)
     } else {
