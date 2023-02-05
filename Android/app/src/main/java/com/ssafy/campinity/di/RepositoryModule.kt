616f8b1ae2036e64a0a482f19e8dd7375e4da5c7
dobbi030 <dmstjs7047@hanmail.net>
2023-02-06 04:04:05 +0900

    [CHORE] merge conflict

--- Android/app/src/main/java/com/ssafy/campinity/di/RepositoryModule.kt
+++ Android/app/src/main/java/com/ssafy/campinity/di/RepositoryModule.kt
@@ -1,12 +1,11 @@
 package com.ssafy.campinity.di
 
-import com.ssafy.campinity.data.remote.datasource.communitycampsite.CommunityRemoteDataSourceImpl
+import com.ssafy.campinity.data.remote.datasource.CommunityCampsite.CommunityRemoteDataSourceImpl
 import com.ssafy.campinity.data.remote.datasource.auth.AuthRemoteDataSourceImpl
 import com.ssafy.campinity.data.remote.datasource.collection.CollectionRemoteDataSourceImpl
 import com.ssafy.campinity.data.remote.datasource.curation.CurationRemoteDataSourceImpl
 import com.ssafy.campinity.data.remote.datasource.home.HomeRemoteDataSourceImpl
 import com.ssafy.campinity.data.remote.datasource.mypage.MyPageRemoteDataSourceImpl
-import com.ssafy.campinity.data.remote.datasource.user.UserRemoteDataSourceImpl
 import com.ssafy.campinity.data.remote.datasource.note.NoteRemoteDataSourceImpl
 import com.ssafy.campinity.data.remote.datasource.search.SearchRemoteDataSourceImpl
 import com.ssafy.campinity.data.remote.datasource.user.UserRemoteDataSourceImpl
