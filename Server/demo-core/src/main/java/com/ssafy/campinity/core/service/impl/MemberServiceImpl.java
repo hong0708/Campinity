6add7282db19590d2dca99ba9777a076bba3f778
구미_6반_권용준 <ubermen5ch1308@gmail.com>
2023-02-06 00:15:25 +0900

    [FEAT] 회원가입 도중 이탈시 처리 API 추가

--- Server/demo-core/src/main/java/com/ssafy/campinity/core/service/impl/MemberServiceImpl.java
+++ Server/demo-core/src/main/java/com/ssafy/campinity/core/service/impl/MemberServiceImpl.java
@@ -3,9 +3,14 @@ package com.ssafy.campinity.core.service.impl;
 import com.ssafy.campinity.core.entity.member.Member;
 import com.ssafy.campinity.core.repository.member.MemberRepository;
 import com.ssafy.campinity.core.service.MemberService;
+import com.ssafy.campinity.core.utils.ErrorMessageEnum;
 import lombok.RequiredArgsConstructor;
+import org.springframework.dao.OptimisticLockingFailureException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
 import java.util.NoSuchElementException;
 import java.util.UUID;
 
@@ -14,15 +19,16 @@ import java.util.UUID;
 public class MemberServiceImpl implements MemberService {
 
     private final MemberRepository memberRepository;
+    private final EntityManager em;
 
     @Override
     public Member findMemberById(Integer id) {
-        return memberRepository.findById(id).orElseThrow(NoSuchElementException::new);
+        return memberRepository.findById(id).orElseThrow(() -> new NoSuchElementException(ErrorMessageEnum.USER_NOT_EXIST.getMessage()));
     }
 
     @Override
     public Member findMemberByUUID(UUID uuid) {
-        return memberRepository.findMemberByUuidAndExpiredIsFalse(uuid).orElseThrow(NoSuchElementException::new);
+        return memberRepository.findMemberByUuidAndExpiredIsFalse(uuid).orElseThrow(() -> new NoSuchElementException(ErrorMessageEnum.USER_NOT_EXIST.getMessage()));
     }
 
     @Override
@@ -33,5 +39,21 @@ public class MemberServiceImpl implements MemberService {
     @Override
     public Boolean checkNicknameDuplicate(String nickname) {
         return memberRepository.existsByName(nickname);
+    }
+
+    @Override
+    @Transactional
+    public Integer deleteMemberHard(Integer memberId) {
+        try {
+            String jpql = "delete from Member m where m.id =:memberId";
+            Query query = em.createQuery(jpql);
+            query.setParameter("memberId", memberId);
+            Integer rows = query.executeUpdate();
+            return rows;
+        } catch (OptimisticLockingFailureException e) {
+            throw new OptimisticLockingFailureException(e.getMessage());
+        } catch (IllegalArgumentException e) {
+            throw new IllegalArgumentException(e.getMessage());
+        }
     }
 }
