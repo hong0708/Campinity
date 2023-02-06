8956de2086f257805b27533212a88c445eb80325
윤세림 <tpfla9953@naver.com>
2023-02-06 12:20:50 +0900

    [FEAT] 마이페이지 내 정보 받아오기 구현

--- Android/app/src/main/java/com/ssafy/campinity/data/remote/service/MyPageApiService.kt
+++ Android/app/src/main/java/com/ssafy/campinity/data/remote/service/MyPageApiService.kt
@@ -1,10 +1,14 @@
 package com.ssafy.campinity.data.remote.service
 
 import com.ssafy.campinity.data.remote.datasource.mypage.MyPageNoteResponse
+import com.ssafy.campinity.data.remote.datasource.mypage.MyPageUserResponse
 import retrofit2.http.GET
 
 interface MyPageApiService {
 
     @GET("/api/v2/messages/my-messages")
     suspend fun getNotes(): MyPageNoteResponse
+
+    @GET("/api/v4/members/profiles")
+    suspend fun getUserInfo(): MyPageUserResponse
 }
