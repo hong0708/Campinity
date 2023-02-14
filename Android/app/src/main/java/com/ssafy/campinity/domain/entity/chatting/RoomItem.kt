a15398c534dd06ccf8875d189775e732f36ebeaa
윤세림 <tpfla9953@naver.com>
2023-02-14 17:49:06 +0900

    [FEAT] 소켓 통신 기본 설정 코드 구현

--- Android/app/src/main/java/com/ssafy/campinity/domain/entity/chatting/RoomItem.kt
+++ Android/app/src/main/java/com/ssafy/campinity/domain/entity/chatting/RoomItem.kt
@@ -1,6 +1,7 @@
 package com.ssafy.campinity.domain.entity.chatting
 
 data class RoomItem(
+    val roomId: String,
     val title: String,
     val subject: String,
     val otherProfileImg: String,
---
+++ Android/app/src/main/java/com/ssafy/campinity/presentation/chatting/ChattingViewModel.kt
@@ -0,0 +1,12 @@
+package com.ssafy.campinity.presentation.chatting
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class ChattingViewModel @Inject constructor(
+
+) : ViewModel() {
+
+}
