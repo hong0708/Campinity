7d76ae4c97535f6141ba0ee71f80c6c83faffc76
윤세림 <tpfla9953@naver.com>
2023-02-16 10:51:12 +0900

    [CHORE] 도움 요청 응답 확인시 lottie 애니메이션 추가

--- Android/app/src/main/java/com/ssafy/campinity/presentation/community/campsite/ReceiveHelpNoteActivity.kt
+++ Android/app/src/main/java/com/ssafy/campinity/presentation/community/campsite/ReceiveHelpNoteActivity.kt
@@ -1,10 +1,13 @@
 package com.ssafy.campinity.presentation.community.campsite
 
+import android.animation.Animator
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import com.ssafy.campinity.ApplicationClass
+import com.ssafy.campinity.R
 import com.ssafy.campinity.common.util.showToastMessage
 import com.ssafy.campinity.databinding.ActivityReceiveHelpNoteBinding
 import com.ssafy.campinity.presentation.MainActivity
@@ -39,13 +42,15 @@ class ReceiveHelpNoteActivity : AppCompatActivity() {
                     showToastMessage("다음 기회를 노려보세요!")
                     val i = Intent(this, MainActivity::class.java)
                     i.putExtra("state", "no_assigned")
-                    i.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_CLEAR_TOP
+                    i.flags =
+                        Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_CLEAR_TOP
                     startActivity(i)
                     finish()
                 } else {
                     showToastMessage("선정되었습니다!")
                     val i = Intent(this, ChatListActivity::class.java)
-                    i.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_CLEAR_TOP
+                    i.flags =
+                        Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_CLEAR_TOP
                     startActivity(i)
                     finish()
                 }
@@ -55,10 +60,31 @@ class ReceiveHelpNoteActivity : AppCompatActivity() {
                 ApplicationClass.preferences.fcmToken.toString()
             )
         }
+
         binding.btnCancel.setOnClickListener {
             val i = Intent(this, CommunityActivity::class.java)
-            i.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_CLEAR_TOP
+            i.flags =
+                Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_CLEAR_TOP
             startActivity(i)
+        }
+
+        binding.lottieReceiveHelpNote.apply {
+            addAnimatorListener(object : Animator.AnimatorListener {
+                override fun onAnimationStart(p0: Animator?) {}
+
+                override fun onAnimationEnd(p0: Animator?) {
+                    binding.clLottieHelpNote.visibility = View.GONE
+                    binding.clReceiveHelpNote.visibility = View.VISIBLE
+                }
+
+                override fun onAnimationCancel(p0: Animator?) {}
+
+                override fun onAnimationRepeat(p0: Animator?) {}
+            })
+            setAnimation(R.raw.fcm_letter)
+            speed = 0.5f
+            visibility = View.VISIBLE
+            playAnimation()
         }
     }
 }
